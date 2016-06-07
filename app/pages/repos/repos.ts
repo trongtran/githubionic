488202002a3bb02067aca1c991f302f5e13b41d3
gangachris <ganga.chris@gmail.com>
2016-06-08 00:55:39 +0300

    Finished ionic starter

---
+++ app/pages/repos/repos.ts
@@ -0,0 +1,14 @@
+import {Page, NavController} from 'ionic-angular';
+
+/*
+  Generated class for the ReposPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/repos/repos.html',
+})
+export class ReposPage {
+  constructor(public nav: NavController) {}
+}
---
+++ app/pages/user-details/user-details.ts
@@ -0,0 +1,34 @@
+// Add NavParams to get the naviagtion parameters
+import {Page, NavController, NavParams} from 'ionic-angular';
+
+// Import GithubUsers Provider
+import {GithubUsers} from '../../providers/github-users/github-users';
+
+// Import the User model
+import {User} from '../../models/user';
+
+/*
+  Generated class for the UserDetailsPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/user-details/user-details.html',
+
+  //Add githubusers provider
+})
+export class UserDetailsPage {
+  user: User = new User;
+  login: string;
+
+
+  constructor(public nav: NavController, navParams: NavParams, githubUsers: GithubUsers) {
+    // Retrieve the login from the navigation parameters
+    this.login = navParams.get('login');
+
+    // Get the user details and log
+    githubUsers.loadDetails(this.login)
+      .then( user => this.user = user)
+  }
+}
